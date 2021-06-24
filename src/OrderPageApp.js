2c5663aa065434a1607825a5cd21d07d1f908ebe
mushjoon <kimhyjun@umich.edu>
2021-06-24 14:33:18 +0900

    주문페이지 수정중

---
+++ src/OrderPageApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ProductInfo from "./purchase/component/ProductInfo";
+
+
+function OrderPageApp() {
+  return (
+    <div className = "container">
+      {/* <TopSection />
+      <MemberInfo />
+      <ProductCategory />
+      <ProductInfo />
+      <BottomSection /> */}
+      <ProductInfo />
+    </div>
+  );
+}
+
+export default OrderPageApp;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './purchase.css';
+import OrderPageApp from './OrderPageApp';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <OrderPageApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
