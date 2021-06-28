1efeac2d4cfa75af7221bf2d96d1ac5d8d32f555
otw7917 <57518724+otw7917@users.noreply.github.com>
2021-06-28 15:01:12 +0900

    Feature/mycopang  (#19)
    * 패키지 통합 처리
    
    * package, package-lock ignore처리 추가
    
    * 마이쿠팡 페이지, 쿠페이 관련 파일 + css 4개
    
    * 다시 4개 파일 ignore 수정

--- src/RouteMain.js
+++ src/RouteMain.js
@@ -4,6 +4,7 @@ import Cart from './purchase/Cart';
 import Menu from './Menu';
 //import ProductListApp from './youngjae/ProductListApp';
 //import OrderPageApp from './purchase/OrderPageApp';
+import MyCopang from "./member/MyCopang";
 
 const RouteMain = () =>{
     return (
@@ -14,7 +15,7 @@ const RouteMain = () =>{
                 {/* <Route path="/member/2" component={OrderPageApp}/> */}
                 {/* <Route path="/member/3" component={sds}/> */}
                 {/* <Route path="/member/4" component={ProductListApp}/> */}
-                <Route path="/member/5" component={RouteHyunjin}/>
+                <Route path="/member/5" component={MyCopang}/>
                 <Route path="/member/6" component={RouteHyunjin}/>
                 <Route path="/member/7" component={RouteHyunjin}/>
             </BrowserRouter>
