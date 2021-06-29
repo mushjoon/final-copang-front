2bce2746c2861f7e6832f4263ba0bfb94b00cb70
yjs24 <yunjs24@gmail.com>
2021-06-29 17:15:04 +0900

    app.js provider added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import RouteMain from './RouteMain';
 
+import Reducer from './_reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 //App.js 수정 다시함 또다시함
-const App=()=>{
-    return(
-        <Router>
-            <RouteMain/>
-        </Router>
-    )
+const App = () => {
+    return (
+        <Provider
+            store={createStoreWithMiddleware(
+                Reducer,
+                window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+        >
+
+            <Router>
+                <RouteMain />
+            </Router>
+        </Provider>)
 }
 
 export default App;
