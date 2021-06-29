6b5abea57f63ecb46af653a22a33db42b856847f
mindchj <78585162+mindchj@users.noreply.github.com>
2021-06-29 11:22:50 +0900

    Hyunjin2 (#36)
    * deleted commit
    
    * aaa
    
    * asdfasdf
    
    * app.js 수정
    
    * 수정 다시함
    
    * 또다시함
    
    * asdfafsd
    
    * header folder add
    
    * 콘솔로그 주석 수정
    
    * warning 제거
    
    * warning 메세지 제거
    
    * Menu.js 수정

--- src/hyunjin/Menu.js
+++ src/hyunjin/Menu.js
@@ -1,52 +1,17 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Menu = () =>{
-    const [input, setInput] = useState();
-    const [loginid, setLoginid] = useState();
-
-    const inputChange = (e) => {
-        setInput(e.target.value);
-    }
-
-    const login = (id) => {
-        sessionStorage.setItem("loginid",id);
-        setLoginid(id);
-        console.log(id+"로 로그인");
-    }
-    const logout = () => {
-        sessionStorage.clear();
-        setLoginid('');
-        console.log("로그아웃됨");
-    }
-    const getLoginState = () => {
-        return sessionStorage.getItem("loginid");
-    }
-
     return (
         <div>
             <ul>
                 <li>
-                    <Link to="/test">테스트 페이지</Link>
-                </li>
-                <li>
-                    <Link to="/test22/13">Test22 페이지</Link>
-                </li>
-                <li>
-                    <Link to="/list">상품 리스트</Link>
+                    <Link to="/test">장바구니 테스트</Link>
                 </li>
                 <li>
                     <Link to="/cart">장바구니</Link>
                 </li>
                 <li>
-                    <input onChange={inputChange}></input>
-                    <button onClick={() => login(input)}>로그인</button>
-                </li>
-                <li>
-                    <button onClick={logout}>로그아웃</button>
-                </li>
-                <li>
-                    로그인 상태 : {getLoginState() ? getLoginState() : "로그아웃됨"}
+                    <Link to="/test22/13">Test22 페이지</Link>
                 </li>
             </ul>
             <hr/>
