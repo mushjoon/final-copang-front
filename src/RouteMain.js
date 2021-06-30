7554604c50b8c60e2c74787e9cfe6127b6afb439
dudwo1996 <83436442+dudwo1996@users.noreply.github.com>
2021-06-30 10:23:28 +0900

    Hotfix2 (#47)
    * mmmm
    
    * 테스트중
    
    * index.js commit
    
    * 풀받기위한 나머지 커밋
    
    * 마스터와 동기화
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * day 0628 last commit
    
    * commit
    
    * material-UI, purchase로 history
    
    * productlistapp+orderpageapp
    
    * product 라우터 설정
    
    * product.css수정
    
    * 공용 라우터,productRoute 수정(member/4/추가)

--- src/RouteMain.js
+++ src/RouteMain.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import RouteHyunjin from "./hyunjin/RouteHyunjin";
 import Cart from "./purchase/Cart";
 import Menu from "./Menu";
-import ProductListApp from "./youngjae/ProductListApp";
+import ProductListRouteMain from './youngjae/ProductListRouteMain';
 //import OrderPageApp from "./purchase/OrderPageApp";
 import MyCopang from "./member/MyCopang";
 import AddNewProductApp from "./product/AddNewProduct/AddNewProductApp";
@@ -15,7 +15,7 @@ const RouteMain = () => {
         <Route path="/member/1" component={Cart} />
         <Route path="/member/2" component={AddNewProductApp} />
         {/* <Route path="/member/3" component={sds}/> */}
-        <Route path="/member/4" component={ProductListApp} />
+        <Route path="/member/4" component={ProductListRouteMain} />
         <Route path="/member/5" component={MyCopang} />
         <Route path="/member/6" component={RouteHyunjin} />
         <Route path="/member/7" component={RouteHyunjin} />
--- src/youngjae/ProductListRouteMain.js
+++ src/youngjae/ProductListRouteMain.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import ProductList from '../product/ProductList&Detail/ProductList';
 import ProductDetail from '../product/ProductList&Detail/ProductDetail';
-import test from './test';
+// import test from './test';
 import ProductDescBottom from '../product/ProductList&Detail/ProductDescBottom';
 import ProductReviewBottom from '../product/ProductList&Detail/ProductReviewBottom';
 import ProductQuestionBottom from '../product/ProductList&Detail/ProductQuestionBottom';
@@ -14,12 +14,12 @@ const ProductListRouteMain = () =>{
             <Route exact path="/member/4" component={ProductList}/>
             <Route exact path="/" component={ProductList}/>
             <Route path="/list" component={ProductList}/>
-            <Route path="/product/selectOne/:sid" component={ProductDetail}/>
-            <Route path="/product/selectOne/:sid/ProductDescBottom" component={ProductDescBottom}/>
-            <Route path="/product/selectOne/:sid/ProductReviewBottom" component={ProductReviewBottom}/>
-            <Route path="/product/selectOne/:sid/ProductQuestionBottom" component={ProductQuestionBottom}/>
-            <Route path="/purchase/product/" component={test}/>
-            <Route path="/member/2" component={OrderPageApp} />
+            <Route path="/member/4/product/selectOne/:sid" component={ProductDetail}/>
+            <Route path="/member/4/product/selectOne/:sid/ProductDescBottom" component={ProductDescBottom}/>
+            <Route path="/member/4/product/selectOne/:sid/ProductReviewBottom" component={ProductReviewBottom}/>
+            <Route path="/member/4/product/selectOne/:sid/ProductQuestionBottom" component={ProductQuestionBottom}/>
+            {/* <Route path="/purchase/product/" component={test}/> */}
+            <Route path="/member/4/orderpage" component={OrderPageApp} />
         </div>
     )
 }
