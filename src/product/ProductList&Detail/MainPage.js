c5e71e23310580518b95af5df5f701375d68d209
dudwo1996 <83436442+dudwo1996@users.noreply.github.com>
2021-07-08 12:10:40 +0900

    Hotfix2 (#107)
    * mmmm
    
    * 테스트중
    
    * index.js commit
    
    * 풀받기위한 나머지 커밋
    
    * 마스터와 동기화
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * day 0628 last commit
    
    * commit
    
    * material-UI, purchase로 history
    
    * productlistapp+orderpageapp
    
    * product 라우터 설정
    
    * product.css수정
    
    * 공용 라우터,productRoute 수정(member/4/추가)
    
    * backend API 연동
    
    * 백엔드랑 연결작업
    
    * 장바구니기능 success
    
    * NumberFormat add
    
    * reviewWriteForm add commit
    
    * productDetail + orderPage commi
    
    * reviewWrite add
    
    * 리뷰작성 주문페이지 합침
    
    * reviewWrite 완성
    
    * ProductDetail.js 수정
    
    * productDetail.js 주문페이지로 보내는 데이터 수정
    
    * css수정 commit
    
    * 문의 등록,리스트 불러오기 기능 commit
    
    * product review and question commit
    
    * ProductReviewWriteForm 수정 commit
    
    * productReview commit
    
    * commit!!

--- src/product/ProductList&Detail/MainPage.js
+++ src/product/ProductList&Detail/MainPage.js
@@ -4,14 +4,15 @@ import './Product.css';
 const MainPage = () => {
     return(
         <div style={{width:'100%',height:'100%'}}>
+            <strong style={{fontSize:'20pt'}}>오늘의 발견</strong>&nbsp;&nbsp;<span style={{fontSize:'15pt',color:'#777'}}> | COPANG이 엄선한 가장 HOT한 상품!</span>
             <ul className="MainImage" style={{float:'left'}}>
-                <li><img style={{width:'400px',height:'500px'}} alt="" src="https://static.alconn.co/image/984ade40-745d-4138-9997-c7fda3493f2e"/></li>
+                <li><img style={{width:'400px',height:'508px'}} alt="" src="https://static.alconn.co/image/984ade40-745d-4138-9997-c7fda3493f2e"/></li>
                 <li><img style={{width:'600px',height:'250px'}} alt="" src="https://static.alconn.co/image/00130026-b9fe-45a9-a5eb-732f016c246d"/></li>
                 <li><img style={{width:'250px',height:'250px'}} alt="" src="https://static.alconn.co/image/b43e2172-05d0-4bfe-a38c-6360325973de"/></li>
                 <li><img style={{width:'250px',height:'250px'}} alt="" src="https://static.alconn.co/image/03ae243d-dff0-43c7-8f98-b2489e7c6a40"/></li>
                 <li><img style={{width:'600px',height:'250px'}} alt="" src="https://static.alconn.co/image/d40b17f5-8dd5-4b56-b3f4-842f31338492"/></li>
-                <li><img style={{width:'620px',height:'500px'}} alt="" src="https://static.alconn.co/image/11904a7c-b750-4fce-89e1-8ecf78e2cf58"/></li>
-                <li><img style={{width:'620px',height:'500px'}}alt="" src="https://static.alconn.co/image/d24bcb83-01fc-445a-8139-4a0159b9ad6d"/></li>
+                <li><img style={{width:'628px',height:'500px'}} alt="" src="https://static.alconn.co/image/11904a7c-b750-4fce-89e1-8ecf78e2cf58"/></li>
+                <li><img style={{width:'628px',height:'500px'}} alt="" src="https://static.alconn.co/image/d24bcb83-01fc-445a-8139-4a0159b9ad6d"/></li>
             </ul>
         </div>
     )
