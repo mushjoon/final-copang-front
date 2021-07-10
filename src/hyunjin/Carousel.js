28ecc90f5e7f4fc7c16a7a6e307263b453955834
apfp77 <apfp77@naver.com>
2021-07-10 16:02:23 +0900

    package-json add

--- src/hyunjin/Carousel.js
+++ src/hyunjin/Carousel.js
@@ -1,43 +1,41 @@
-import React from 'react';
-import p1 from './01.png';
-import p2 from './02.png';
-import p3 from './03.png';
+import React from "react";
+import p1 from "./01.png";
+import p2 from "./02.png";
+import p3 from "./03.png";
 const Carousel = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
 
-            <div id="demo" class="carousel slide" data-ride="carousel">
-
-            
-            <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-            </ul>
-
-
-            <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src={p1} alt="Los Angeles" width="1100" height="500"/>
-            </div>
-            <div class="carousel-item">
-                <img src={p2} alt="Chicago" width="1100" height="500"/>
-            </div>
-            <div class="carousel-item">
-                <img src={p3} alt="New York" width="1100" height="500"/>
-            </div>
-            </div>
-
-
-            <a class="carousel-control-prev" href="#demo" data-slide="prev">
-            <span class="carousel-control-prev-icon" style={{color:'red'}}></span>
-            </a>
-            <a class="carousel-control-next" href="#demo" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-            </a>
-            </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={p1} alt="Los Angeles" width="1100" height="500" />
+          </div>
+          <div class="carousel-item">
+            <img src={p2} alt="Chicago" width="1100" height="500" />
+          </div>
+          <div class="carousel-item">
+            <img src={p3} alt="New York" width="1100" height="500" />
+          </div>
         </div>
-    );
+
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span
+            class="carousel-control-prev-icon"
+            style={{ color: "red" }}
+          ></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
--- src/product/ProductList&Detail/MainPage.js
+++ src/product/ProductList&Detail/MainPage.js
@@ -26,7 +26,7 @@ const MainPage = ({ history }) => {
   useEffect(() => {
     const res = async () => {
       const result = await axios.get(
-        "https://alconn.co/api/item/list/categoryid=" + FURNITURE_CATEGORY_ID
+        "http://localhost:8080/api/item/list/categoryid=" + 2
       );
       setFuniture(result.data.data);
       console.log(result.data);
@@ -234,122 +234,120 @@ const MainPage = ({ history }) => {
         <br />
         {Furniture &&
           Furniture.map((row, idx) => {
-            if (idx >= 5) return;
-            else
-              return (
-                <ul
-                  className="searchproduct"
+            // if (idx >= 5) return;
+            // else
+            return (
+              <ul
+                className="searchproduct"
+                style={{
+                  listStyle: "none",
+                  margin: "0",
+                  padding: "0",
+                  float: "left",
+                }}
+              >
+                <li
+                  row={row}
+                  key={idx}
                   style={{
-                    listStyle: "none",
-                    margin: "0",
-                    padding: "0",
-                    float: "left",
+                    marginRight: "25px",
+                    width: "230px",
+                    display: "inline-block",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    history.push(
+                      "/product/selectOne/" + row.itemId + "/ProductDescBottom"
+                    );
                   }}
                 >
-                  <li
-                    row={row}
-                    key={idx}
-                    style={{
-                      marginRight: "25px",
-                      width: "230px",
-                      display: "inline-block",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => {
-                      history.push(
-                        "/product/selectOne/" +
-                          row.itemId +
-                          "/ProductDescBottom"
-                      );
-                    }}
-                  >
-                    <dl style={{ marginTop: "0", marginBottom: "1rem;" }}>
-                      <dt>
-                        <img
-                          alt={row.mainImg}
-                          src={row.mainImg}
-                          style={{ width: "230px", height: "230px" }}
-                        />
-                      </dt>
-                      <dd className="desc">
-                        <div>
-                          <div className="namedesc">
-                            <div className="name" style={{ width: "230px" }}>
-                              {row.itemName}
-                            </div>
-                          </div>
-                          <div className="price-area">
-                            <em className="sale">
-                              <strong className="price-value">
-                                {numberFormat(row.price)}
-                              </strong>
-                              원
-                            </em>
-                            {Math.round(row.averageRating) === 1 ? (
-                              <div>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <span className="ReviewCount">
-                                  ({row.countReviews})
-                                </span>
-                              </div>
-                            ) : Math.round(row.averageRating) === 2 ? (
-                              <div>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <span className="ReviewCount">
-                                  ({row.countReviews})
-                                </span>
-                              </div>
-                            ) : Math.round(row.averageRating) === 3 ? (
-                              <div>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="emptyStar" />
-                                <StarIcon className="emptyStar" />
-                                <span className="ReviewCount">
-                                  ({row.countReviews})
-                                </span>
-                              </div>
-                            ) : Math.round(row.averageRating) === 4 ? (
-                              <div>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="emptyStar" />
-                                <span className="ReviewCount">
-                                  ({row.countReviews})
-                                </span>
-                              </div>
-                            ) : Math.round(row.averageRating) === 5 ? (
-                              <div>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <StarIcon className="smstar"></StarIcon>
-                                <span className="ReviewCount">
-                                  ({row.countReviews})
-                                </span>
-                              </div>
-                            ) : (
-                              <div></div>
-                            )}
+                  <dl style={{ marginTop: "0", marginBottom: "1rem;" }}>
+                    <dt>
+                      <img
+                        alt={row.mainImg}
+                        src={row.mainImg}
+                        style={{ width: "230px", height: "230px" }}
+                      />
+                    </dt>
+                    <dd className="desc">
+                      <div>
+                        <div className="namedesc">
+                          <div className="name" style={{ width: "230px" }}>
+                            {row.itemName}
                           </div>
                         </div>
-                      </dd>
-                    </dl>
-                  </li>
-                </ul>
-              );
+                        <div className="price-area">
+                          <em className="sale">
+                            <strong className="price-value">
+                              {numberFormat(row.price)}
+                            </strong>
+                            원
+                          </em>
+                          {Math.round(row.averageRating) === 1 ? (
+                            <div>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <span className="ReviewCount">
+                                ({row.countReviews})
+                              </span>
+                            </div>
+                          ) : Math.round(row.averageRating) === 2 ? (
+                            <div>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <span className="ReviewCount">
+                                ({row.countReviews})
+                              </span>
+                            </div>
+                          ) : Math.round(row.averageRating) === 3 ? (
+                            <div>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="emptyStar" />
+                              <StarIcon className="emptyStar" />
+                              <span className="ReviewCount">
+                                ({row.countReviews})
+                              </span>
+                            </div>
+                          ) : Math.round(row.averageRating) === 4 ? (
+                            <div>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="emptyStar" />
+                              <span className="ReviewCount">
+                                ({row.countReviews})
+                              </span>
+                            </div>
+                          ) : Math.round(row.averageRating) === 5 ? (
+                            <div>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <StarIcon className="smstar"></StarIcon>
+                              <span className="ReviewCount">
+                                ({row.countReviews})
+                              </span>
+                            </div>
+                          ) : (
+                            <div></div>
+                          )}
+                        </div>
+                      </div>
+                    </dd>
+                  </dl>
+                </li>
+              </ul>
+            );
           })}
       </div>
       <br />
