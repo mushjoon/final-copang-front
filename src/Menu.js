ab0927171ad1a8ddd70c33bc4a31f0d68e133faf
apfp77 <78768234+apfp77@users.noreply.github.com>
2021-07-13 16:29:05 +0900

    Mypage3 (#156)
    * Menu수정

--- src/Menu.js
+++ src/Menu.js
@@ -2,16 +2,33 @@ import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        marginLeft: "100px",
+        fontWeight: "bold",
+      }}
+    >
       {/* <Link to="/member/1">장바구니</Link>&nbsp;&nbsp; */}
-      <Link to="/member/2">판매자 상품등록</Link>&nbsp;&nbsp;
+      {/* <Link to="/member/2">판매자 상품등록</Link>&nbsp;&nbsp; */}
       {/* <Link to="/member/3">MainPage(작업중)</Link>&nbsp;&nbsp; */}
-      <Link to="/product">상품 리스트</Link>&nbsp;/&nbsp;
-      <Link to="/product/header/hot">HOT! 인기 상품</Link>&nbsp;/&nbsp;
-      <Link to="/product/header/free">무료 배송 상품</Link>&nbsp;/&nbsp;
-      <Link to="/product/header/review">리뷰 많은 상품</Link>&nbsp;/&nbsp;
-      <Link to="/product/header/new">최신 등록 상품</Link>&nbsp;/&nbsp;
-      <Link to="/product/header/display">오늘의 기획전</Link>&nbsp;&nbsp;
+      <div>
+        <Link to="/product">상품 리스트</Link>
+      </div>
+      <div>
+        <Link to="/product/header/hot">HOT! 인기 상품</Link>
+      </div>
+      <div>
+        <Link to="/product/header/free">무료 배송 상품</Link>
+      </div>
+      <div>
+        <Link to="/product/header/review">리뷰 많은 상품</Link>
+      </div>
+      <div>
+        <Link to="/product/header/new">최신 등록 상품</Link>
+      </div>
+      {/* <Link to="/product/header/display">오늘의 기획전</Link>&nbsp;&nbsp; */}
       {/* <Link to="/member/5">마이 페이지</Link> */}
       <br />
       {/* <Link to="/member/6">(Test)상품 추가폼</Link>&nbsp;&nbsp; */}
@@ -23,4 +40,3 @@ const Menu = () => {
   );
 };
 export default Menu;
-
