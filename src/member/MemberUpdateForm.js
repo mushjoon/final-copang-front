6a750e0f88fffe8a5ac194b04e669a45502e6ca4
apfp77 <78768234+apfp77@users.noreply.github.com>
2021-07-13 18:02:30 +0900

    Mypage3 (#161)
    * updateFormCss

--- src/member/MemberUpdateForm.js
+++ src/member/MemberUpdateForm.js
@@ -3,49 +3,96 @@ import { Grid, Typography, TextField, Button } from "@material-ui/core";
 import axios from "axios";
 
 const MemberUpdateForm = (props) => {
-    let userName = props.location.state.userId.username;
-    let description = props.location.state.userId.description;
-    let phone = props.location.state.userId.phone;
-    let profileImage = props.location.state.userId.profileImage;
-    let realName = props.location.state.userId.realName;
-    let role = props.location.state.userId.role;
-    let signInDate = props.location.state.userId.signInDate;
+  let userName = props.location.state.userId.username;
+  let description = props.location.state.userId.description;
+  let phone = props.location.state.userId.phone;
+  let profileImage = props.location.state.userId.profileImage;
+  let realName = props.location.state.userId.realName;
+  let role = props.location.state.userId.role;
+  let signInDate = props.location.state.userId.signInDate;
 
+  const [password, setPassword] = useState();
 
-    const [password, setPassword] = useState();
+  const [info, setInfo] = useState({
+    username: userName,
+    password: "",
+    realName: realName,
+    phone: phone,
+    description: description,
+  });
 
-    const [info, setInfo] = useState({username : userName, password : "", realName : realName,phone : phone, description : description});
+  const handleInfoChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+    // console.log(info);
+  };
 
-    const handleInfoChange = (e) => {
-        const { name, value } = e.target;
-        setInfo({ ...info, [name]: value });
-        // console.log(info);
-    };
+  const handleSubmit = async () => {
+    const putData = await axios
+      .put("https://alconn.co/api/user", info)
+      .then(() => props.history.push({ pathname: "/mycopang/userinfo" }));
+  };
+  return (
+    <div>
+      <h2 style={{ textAlign: "center", paddingTop: "5%" }}>
+        {userName} 님 정보 수정하기
+      </h2>
+      <div
+        style={{
+          paddingLeft: "30%",
+          paddingRight: "30%",
+        }}
+      >
+        <h5 style={{ paddingTop: "7%" }}>유저이름</h5>
 
-    const handleSubmit = async () => {
-        const putData = await axios.put("https://alconn.co/api/user",info)
-        .then(()=> props.history.push({pathname : '/mycopang/userinfo'}));
-    }
-    return (
-        <div>
-            <Typography variant="h2" gutterBottom>
-                {userName} 님 정보 수정하기
-            </Typography>
-            <h4>유저이름</h4>
-            <input type="text" name = "realName" value={info.realName} onChange={handleInfoChange}/>
+        <TextField
+          required
+          type="tel"
+          name="realName"
+          value={info.realName}
+          fullWidth
+          onChange={handleInfoChange}
+        />
 
-            <h4>전화번호</h4>
-            <input type="tel" name = "phone" value={info.phone} onChange={handleInfoChange}/>
+        <h5 style={{ paddingTop: "7%" }}>전화번호</h5>
+        <TextField
+          required
+          type="tel"
+          name="phone"
+          value={info.phone}
+          fullWidth
+          onChange={handleInfoChange}
+        />
+        <h5 style={{ paddingTop: "7%" }}>설명 </h5>
 
-            <h4>설명 </h4>
-            <input type="text" name = "description" value={info.description} onChange={handleInfoChange}/>
+        <TextField
+          required
+          type="text"
+          name="description"
+          fullWidth
+          onChange={handleInfoChange}
+        />
 
-            <h4> 비밀번호 입력 </h4>
-            <input type="password" name="password" onChange={handleInfoChange} /> 
-
-            <button onClick={()=>handleSubmit()}> 수정하기 </button>
-        </div>
-    )
-}
+        <h5 style={{ paddingTop: "7%" }}> 비밀번호 입력 </h5>
+        <TextField
+          required
+          name="password"
+          type="password"
+          fullWidth
+          onChange={handleInfoChange}
+        />
+        <br />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleSubmit()}
+          style={{ marginTop: "7%" }}
+        >
+          수정하기
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default MemberUpdateForm;
